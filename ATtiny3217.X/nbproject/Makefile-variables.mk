4253858a4004a7b8ab92f0ab3b9428ea516e5b18
sridhar vumma <sridhar6358@gmail.com>
2023-02-02 21:41:01 +0530

    ATtiny3217 Code

---
+++ ATtiny3217.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ATtin3217.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ATtin3217.production.hex
