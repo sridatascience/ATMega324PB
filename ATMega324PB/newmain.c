695d53d08184f58438947c95741b9e84ec7df748
sridhar vumma <50115187+sridatascience@users.noreply.github.com>
2023-04-28 11:01:26 +0530

    Update newmain.c

--- ATMega324PB/newmain.c
+++ ATMega324PB/newmain.c
@@ -1,18 +1,18 @@
 #include <avr/io.h>
 
-volatile int m =10;
-int add(uint8_t n, uint8_t o, uint8_t p ){
+volatile uint8_t m =10;
+volatile uint8_t add(volatile uint8_t n, volatile uint8_t o, volatile uint8_t p ){
    return n+o+p;
 }
 int main() {
-   volatile int a = 0x05;
+   volatile uinit8_t a = 0x05;
    SPCR1 = a;
    m = SPCR1;
-   volatile int c = 0x1F;
+   volatile uint8_t c = 0x1F;
    DDRD = c;
-   volatile int b =0x08;
+   volatile uint8_t b =0x08;
    DDRC = b;
-   volatile int t = add(a,b,c);
+   volatile uint8_t t = add(a,b,c);
    DDRB =t;
    t =a+b+c;
    return 0;
